043496549fa03a7d3f73ff967738d10f88ad5fd4
Nikita Zavadskiy <nick.mr.volk@gmail.com>
2023-05-25 22:04:23 +0200

    add loaders for css and img

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   entry: "./src/index.js",
+  rules: [
+    { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    { test: /\.(png|svg|jpg|jped|gif)$/i, type: "asset/resource" },
+    { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" },
+  ],
 };
