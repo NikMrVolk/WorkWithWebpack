01212b886c2e69d4946d517ba3b6ef4f96e1008c
Nikita Zavadskiy <nick.mr.volk@gmail.com>
2023-06-02 12:09:11 +0200

    add point exit and change module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
-  rules: [
-    { test: /\.css$/, use: ["style-loader", "css-loader"] },
-    { test: /\.(png|svg|jpg|jped|gif)$/i, type: "asset/resource" },
-    { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" },
-  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    __filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|jpg|jped|gif)$/i, type: "asset/resource" },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" },
+    ],
+  },
 };
